38cff834d0b0178691d52809f71b9de9bb7f98b8
Juan Diego Gómez <juandieruiz6@gmail.com>
2021-12-28 14:10:47 +0100

    export/import & models

--- angular-learning/src/app/components/rainbow-six-siege/rainbow-six-siege.component.ts
+++ angular-learning/src/app/components/rainbow-six-siege/rainbow-six-siege.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Configuracion } from 'src/app/models/config';
 @Component({
   selector: 'app-rainbow-six-siege',
   templateUrl: './rainbow-six-siege.component.html',
   styleUrls: ['./rainbow-six-siege.component.css']
 })
 export class RainbowSixSiegeComponent implements OnInit {
-  public titulo: string = "Bienvenidos a Rainbow Six Siege";
-  constructor() { }
+
+  public titulo: string = "Bienvenidos a la aplicacion Angular";
+  public descripcion:string;
+  public config;
+
+  constructor() { 
+    this.config = Configuracion;
+    this.titulo = Configuracion.titulo;
+    this.descripcion = Configuracion.descripcion;
+  }
 
   ngOnInit(): void {
   }
---
+++ angular-learning/src/app/models/config.ts
@@ -0,0 +1,6 @@
+export let Configuracion = {
+    color: "red",
+    fondo: "#ccc",
+    titulo: "Rainbow Six Siege",
+    descripcion: "Temporada de Invierno 2022"
+};
