1c75fc0ae12429498d61eb2987df81f1e27eb68e
Juan Diego Gómez <juandieruiz6@gmail.com>
2021-12-28 11:16:51 +0100

    funciones

---
+++ poo-y-typescript/ts/04-funciones.js
@@ -0,0 +1,6 @@
+function getNumber(numero) {
+    if (numero === void 0) { numero = 12; }
+    return "El numero es: " + numero;
+}
+getNumber(22);
+console.log(getNumber(22));
---
+++ poo-y-typescript/ts/04-funciones.ts
@@ -0,0 +1,7 @@
+
+function getNumber(numero: number = 12): string {
+    return "El numero es: " + numero;
+}
+
+getNumber(22);
+console.log(getNumber(22));
