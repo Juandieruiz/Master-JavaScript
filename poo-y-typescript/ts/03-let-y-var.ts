9e43669055b31ec37c544b1dc5851361dc7c1b82
Juan Diego Gómez <juandieruiz6@gmail.com>
2021-12-28 11:07:12 +0100

    let y var

---
+++ poo-y-typescript/ts/03-let-y-var.ts
@@ -0,0 +1,32 @@
+// Alcance de variables
+// let y var
+
+// let es a nivel de bloque
+
+let master = "Juan";
+let mind = "Perez";
+
+// Var es a nivel de archivo
+
+var numero1 = 10;
+var numero2 = 12;
+
+// !Prueba
+
+if (numero1 == 10) {
+    let numero1 = 44;
+    var numero2 = 55;
+
+    console.log(numero1, numero2);
+}
+console.log(numero1, numero2);
+
+// Las Diferencias entre let y var son:
+// let:
+// - Es a nivel de bloque
+// - Solo se puede usar dentro de un bloque
+// - No se puede redeclarar
+// Var:
+// - Es a nivel de archivo
+// - Se puede redeclarar
+// - Se puede usar fuera de un bloque
