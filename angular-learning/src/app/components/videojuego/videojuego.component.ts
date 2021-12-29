8d1b95221945274db74db00b24089954a7233f9c
Juan Diego Gómez <juandieruiz6@gmail.com>
2021-12-29 13:22:09 +0100

    models

--- angular-learning/src/app/components/videojuego/videojuego.component.ts
+++ angular-learning/src/app/components/videojuego/videojuego.component.ts
@@ -13,24 +13,24 @@ export class VideojuegoComponent implements OnInit, DoCheck, OnDestroy {
     this.titulo = 'Componente de videojuegos';
     this.listado = 'Aquí se mostrará el listado de los videojuegos';
     // Se ejecuta antes de que se cargue el componente
-    console.log('Constructor EJECUTADO...');
+    // console.log('Constructor EJECUTADO...');
   }
 
   ngOnInit(): void {
     // Se ejecuta después de que se cargue el componente
-    console.log("onInit EJECUTADO...");
+    // console.log("onInit EJECUTADO...");
   }
 
   ngDoCheck(): void {
     // Se ejecuta cada vez que se detecta un cambio en el componente
-    console.log("DoCheck EJECUTADO...");
+    // console.log("DoCheck EJECUTADO...");
   }
   ngOnDestroy(): void {
     // Se ejecuta antes de que el componente sea eliminado
-      console.log("OnDestroy EJECUTADO...");
+      // console.log("OnDestroy EJECUTADO...");
   }
 
   cambiarTitulo() {
-    this.titulo = 'Nuevo titulo del componente';
+    this.titulo = 'Bienvenido a Rainbow Six Siege';
   }
 }
