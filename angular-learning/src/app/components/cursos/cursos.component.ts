e1d9ee891a9b895ea53768e8331f4178754177b0
Juan Diego Gómez <juandieruiz6@gmail.com>
2022-01-03 20:48:52 +0100

    redirecciones router navigate

--- angular-learning/src/app/components/cursos/cursos.component.ts
+++ angular-learning/src/app/components/cursos/cursos.component.ts
@@ -20,10 +20,18 @@ export class CursosComponent implements OnInit {
     this._route.params.subscribe((params: Params) =>{
       this.nombre = params['nombre'];
       this.followers = +params['followers'];
-      console.log(this.nombre)
+
+      if (this.nombre == 'gotohome') {
+      this._router.navigate(['/home'])
+        
+      }
+
+
     });
 
-    
   }
 
+  redirigir(){
+    this._router.navigate(['/home'])
+  }
 }
