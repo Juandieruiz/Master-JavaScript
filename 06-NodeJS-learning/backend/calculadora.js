4868cd4aef11712628e52c3506c3c0c2e142d9f8
Juan Diego Gómez <juandieruiz6@gmail.com>
2022-01-07 13:35:36 +0100

    Aplicación de consola con NodeJS

--- 06-NodeJS-learning/backend/calculadora.js
+++ 06-NodeJS-learning/backend/calculadora.js
@@ -1,6 +1,19 @@
 'use strict';
 
+// Recogemos los parámetros de la petición a partir del segundo parámetro
 let params = process.argv.slice(2);
 
-console.log(params);
-console.log("Hola mundo con NodeJS")
+let numero1 = parseInt(params[0]);
+let numero2 = parseInt(params[1]);
+
+let plantilla = `
+    La suma es ${numero1 + numero2}
+    La resta es ${numero1 - numero2}
+    La multiplicación es ${numero1 * numero2}
+    La división es ${numero1 / numero2}
+`;
+
+console.log(plantilla);
+
+console.log("Hola mundo con NodeJS");
+
