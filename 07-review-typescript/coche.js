d08825d1fbbddb5220f51cf9187248576671d125
Juan Diego Gómez <77864382+Juandieruiz@users.noreply.github.com>
2022-01-24 11:24:33 +0100

    constructor

--- 07-review-typescript/coche.js
+++ 07-review-typescript/coche.js
@@ -1,24 +1,38 @@
 // Clase de coche/Molde- POO
 var Coche = /** @class */ (function () {
-    function Coche() {
-        this.velocidad = 0;
-    }
     // constructor de objetos con valores por defecto
-    // constructor(modelo: string, color: string){
-    //     this.modelo = modelo;
-    //     this.color = color;
-    //     this.velocidad = 0;
-    // }
+    function Coche(modelo) {
+        if (modelo === void 0) { modelo = null; }
+        this.modelo = "TESLA GENERICO";
+        this.color = "Blanco";
+        this.velocidad = 0;
+        // comprobamos si el modelo es nulo
+        if (modelo == null) {
+            this.modelo = "TESLA GENERICO";
+        }
+        else {
+            this.modelo = modelo;
+        }
+    }
+    // !GETTER Y SETTER
     // métodos - funciones
+    // ? getModelo - SetModelo
+    Coche.prototype.getModelo = function () {
+        return this.modelo;
+    };
+    Coche.prototype.setModelo = function (modelo) {
+        this.modelo = modelo;
+    };
+    // ? getColor - SetColor
     Coche.prototype.getColor = function () {
         // nos devuelve el valor de la propiedad color
         return this.color;
     };
-    // metodo setter que envia un valor a la propiedad color
     Coche.prototype.setColor = function (color) {
         // el valor asignado viajará a traves de la propiedad color
         this.color = color;
     };
+    // ? Funciones acelerar y frenar
     // mnetodos - funciones a realizar
     Coche.prototype.acelerar = function () {
         this.velocidad++;
@@ -26,6 +40,7 @@ var Coche = /** @class */ (function () {
     Coche.prototype.frenar = function () {
         this.velocidad--;
     };
+    // ? getVelocidad - SetVelocidad
     Coche.prototype.getVelocidad = function () {
         // nos devuelve el valor de la propiedad velocidad
         return this.velocidad;
@@ -47,11 +62,17 @@ coche1.acelerar();
 coche1.acelerar();
 coche2.acelerar();
 coche1.frenar();
+// Asignación de valores modelo a los atributos
+coche1.setModelo("Tesla X");
 // Mostrar valores de los atributos
-console.log("La velocidad del coche 1 es: " + coche1.getVelocidad());
-console.log("La velocidad del coche 2 es: " + coche2.getVelocidad());
-console.log("La velocidad del coche 3 es: " + coche3.getVelocidad());
-console.log('____________________________________________');
 console.log("El color del coche 1 es: " + coche1.getColor());
+console.log("El modelo del coche 1 es: " + coche1.getModelo());
+console.log("La velocidad del coche 1 es: " + coche1.getVelocidad());
+console.log('____________________________________________');
+console.log("La velocidad del coche 2 es: " + coche2.getVelocidad());
+console.log("El modelo del coche 1 es: " + coche1.getModelo());
 console.log("El color del coche 2 es: " + coche2.getColor());
+console.log('____________________________________________');
 console.log("El color del coche 3 es: " + coche3.getColor());
+console.log("El modelo del coche 1 es: " + coche1.getModelo());
+console.log("La velocidad del coche 3 es: " + coche3.getVelocidad());
--- 07-review-typescript/coche.ts
+++ 07-review-typescript/coche.ts
@@ -1,33 +1,51 @@
 // Clase de coche/Molde- POO
 class Coche{
     
-    // atributos - propiedades
+    // Definir atributos - propiedades
     public color:string;
     public modelo: string;
-    public velocidad: number = 0
+    public velocidad: number;
 
 
 
     // constructor de objetos con valores por defecto
-    // constructor(modelo: string, color: string){
-    //     this.modelo = modelo;
-    //     this.color = color;
-    //     this.velocidad = 0;
-    // }
+    constructor(modelo:any = null){
+        this.modelo = "TESLA GENERICO"; 
+        this.color = "Blanco";
+        this.velocidad = 0;
+        
+        // comprobamos si el modelo es nulo
+        if(modelo == null){
+            this.modelo = "TESLA GENERICO";
+        }else{
+            this.modelo = modelo;
+        }
+    }
 
 
+   // !GETTER Y SETTER
     // métodos - funciones
+
+    // ? getModelo - SetModelo
+    public getModelo():string{
+        return this.modelo;
+    }
+
+    public setModelo(modelo:string){
+        this.modelo = modelo;
+    }
+
+    // ? getColor - SetColor
     public getColor(){
         // nos devuelve el valor de la propiedad color
         return this.color;
     }
-    // metodo setter que envia un valor a la propiedad color
     public setColor(color: string){
         // el valor asignado viajará a traves de la propiedad color
         this.color = color;
     }
 
-    
+    // ? Funciones acelerar y frenar
     // mnetodos - funciones a realizar
     public acelerar(){
         this.velocidad++;
@@ -37,6 +55,7 @@ class Coche{
         this.velocidad--;
     }
 
+    // ? getVelocidad - SetVelocidad
     public getVelocidad():number{
         // nos devuelve el valor de la propiedad velocidad
         return this.velocidad;
@@ -62,13 +81,20 @@ coche1.acelerar();
 coche2.acelerar();
 coche1.frenar();
 
-
+// Asignación de valores modelo a los atributos
+coche1.setModelo("Tesla X");
 
 // Mostrar valores de los atributos
-console.log("La velocidad del coche 1 es: " + coche1.getVelocidad());
-console.log("La velocidad del coche 2 es: " + coche2.getVelocidad());
-console.log("La velocidad del coche 3 es: " + coche3.getVelocidad());
-console.log('____________________________________________')
 console.log("El color del coche 1 es: " + coche1.getColor());
+console.log("El modelo del coche 1 es: " + coche1.getModelo());
+console.log("La velocidad del coche 1 es: " + coche1.getVelocidad());
+console.log('____________________________________________');
+
+console.log("La velocidad del coche 2 es: " + coche2.getVelocidad());
+console.log("El modelo del coche 1 es: " + coche1.getModelo());
 console.log("El color del coche 2 es: " + coche2.getColor());
+console.log('____________________________________________');
+
 console.log("El color del coche 3 es: " + coche3.getColor());
+console.log("El modelo del coche 1 es: " + coche1.getModelo());
+console.log("La velocidad del coche 3 es: " + coche3.getVelocidad());
