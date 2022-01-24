584b580e2b21360b2df9c8d49de67c4648eaf944
Juan Diego Gómez <77864382+Juandieruiz@users.noreply.github.com>
2022-01-24 10:42:02 +0100

    methods

--- 07-review-typescript/coche.js
+++ 07-review-typescript/coche.js
@@ -1,7 +1,14 @@
 // Clase de coche/Molde- POO
 var Coche = /** @class */ (function () {
     function Coche() {
+        this.velocidad = 0;
     }
+    // constructor de objetos con valores por defecto
+    // constructor(modelo: string, color: string){
+    //     this.modelo = modelo;
+    //     this.color = color;
+    //     this.velocidad = 0;
+    // }
     // métodos - funciones
     Coche.prototype.getColor = function () {
         // nos devuelve el valor de la propiedad color
@@ -12,17 +19,39 @@ var Coche = /** @class */ (function () {
         // el valor asignado viajará a traves de la propiedad color
         this.color = color;
     };
+    // mnetodos - funciones a realizar
+    Coche.prototype.acelerar = function () {
+        this.velocidad++;
+    };
+    Coche.prototype.frenar = function () {
+        this.velocidad--;
+    };
+    Coche.prototype.getVelocidad = function () {
+        // nos devuelve el valor de la propiedad velocidad
+        return this.velocidad;
+    };
     return Coche;
 }());
 // Creación de Objetos
 var coche1 = new Coche();
 var coche2 = new Coche();
 var coche3 = new Coche();
-// Asignación de valores a los atributos
+// Asignación de valores color a los atributos
 coche1.setColor("Rojo");
 coche2.setColor("Azul");
 coche3.setColor("Verde");
+// Asignación de valores velocidad a los atributos
+coche1.acelerar();
+coche1.acelerar();
+coche1.acelerar();
+coche1.acelerar();
+coche2.acelerar();
+coche1.frenar();
 // Mostrar valores de los atributos
+console.log("La velocidad del coche 1 es: " + coche1.getVelocidad());
+console.log("La velocidad del coche 2 es: " + coche2.getVelocidad());
+console.log("La velocidad del coche 3 es: " + coche3.getVelocidad());
+console.log('____________________________________________');
 console.log("El color del coche 1 es: " + coche1.getColor());
 console.log("El color del coche 2 es: " + coche2.getColor());
 console.log("El color del coche 3 es: " + coche3.getColor());
--- 07-review-typescript/coche.ts
+++ 07-review-typescript/coche.ts
@@ -4,7 +4,17 @@ class Coche{
     // atributos - propiedades
     public color:string;
     public modelo: string;
-    public velocidad: number;
+    public velocidad: number = 0
+
+
+
+    // constructor de objetos con valores por defecto
+    // constructor(modelo: string, color: string){
+    //     this.modelo = modelo;
+    //     this.color = color;
+    //     this.velocidad = 0;
+    // }
+
 
     // métodos - funciones
     public getColor(){
@@ -17,6 +27,21 @@ class Coche{
         this.color = color;
     }
 
+    
+    // mnetodos - funciones a realizar
+    public acelerar(){
+        this.velocidad++;
+    }
+
+    public frenar(){
+        this.velocidad--;
+    }
+
+    public getVelocidad():number{
+        // nos devuelve el valor de la propiedad velocidad
+        return this.velocidad;
+    }
+
 }
 
 // Creación de Objetos
@@ -24,13 +49,26 @@ let coche1 = new Coche();
 let coche2 = new Coche();
 let coche3 = new Coche();
 
-// Asignación de valores a los atributos
+// Asignación de valores color a los atributos
 coche1.setColor("Rojo");
 coche2.setColor("Azul");
 coche3.setColor("Verde");
 
+// Asignación de valores velocidad a los atributos
+coche1.acelerar();
+coche1.acelerar();
+coche1.acelerar();
+coche1.acelerar();
+coche2.acelerar();
+coche1.frenar();
+
+
 
 // Mostrar valores de los atributos
+console.log("La velocidad del coche 1 es: " + coche1.getVelocidad());
+console.log("La velocidad del coche 2 es: " + coche2.getVelocidad());
+console.log("La velocidad del coche 3 es: " + coche3.getVelocidad());
+console.log('____________________________________________')
 console.log("El color del coche 1 es: " + coche1.getColor());
 console.log("El color del coche 2 es: " + coche2.getColor());
 console.log("El color del coche 3 es: " + coche3.getColor());
